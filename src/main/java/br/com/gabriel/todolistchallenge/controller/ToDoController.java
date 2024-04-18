f695df03d305fc6ebfb5e4c3aff7f8ab7c0d5f4b
Gabriel M. Assis <gabrielmedrs@icloud.com>
2024-04-18 15:09:56 -0300

    Creating the routes of controller

--- src/main/java/br/com/gabriel/todolistchallenge/controller/ToDoController.java
+++ src/main/java/br/com/gabriel/todolistchallenge/controller/ToDoController.java
@@ -1,12 +1,33 @@
 package br.com.gabriel.todolistchallenge.controller;
 
+import br.com.gabriel.todolistchallenge.entity.ToDo;
 import br.com.gabriel.todolistchallenge.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
 public class ToDoController {
     @Autowired
     private ToDoService service;
+    @GetMapping
+    public List<ToDo> list(){
+        return service.list();
+    }
+    @PostMapping
+    public List<ToDo> create(@RequestBody ToDo toDo){
+        return service.create(toDo);
+    }
+    @PutMapping
+    public List<ToDo> update(@RequestBody ToDo toDo){
+        return service.update(toDo);
+    }
+    @DeleteMapping("/{id}")
+    public List<ToDo> delete(@PathVariable("id") Long id){
+        return service.delete(id);
+    }
 
 }
