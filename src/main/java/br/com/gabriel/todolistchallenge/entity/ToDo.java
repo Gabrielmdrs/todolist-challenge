6fb36177d2b809406b95dad5707229f01acc8149
Gabriel M. Assis <gabrielmedrs@icloud.com>
2024-04-22 13:14:53 -0300

    refactoring the code

--- src/main/java/br/com/gabriel/todolistchallenge/entity/ToDo.java
+++ src/main/java/br/com/gabriel/todolistchallenge/entity/ToDo.java
@@ -1,21 +1,43 @@
 package br.com.gabriel.todolistchallenge.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 @Entity
 @Table(name = "ToDos")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ToDo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
     private boolean done;
     private int priority;
+
+    public ToDo(String name, String description, boolean done, int priority) {
+        this.name = name;
+        this.description = description;
+        this.done = done;
+        this.priority = priority;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(obj, this);
+    }
+
 
 }
 
