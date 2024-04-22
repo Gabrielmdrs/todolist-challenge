884a6641e2201da90a4e4700ffdf11eb12872a01
Gabriel M. Assis <gabrielmedrs@icloud.com>
2024-04-22 12:49:34 -0300

    Modify response code

--- src/main/java/br/com/gabriel/todolistchallenge/web/ToDoController.java
+++ src/main/java/br/com/gabriel/todolistchallenge/web/ToDoController.java
@@ -1,9 +1,13 @@
-package br.com.gabriel.todolistchallenge.web.controller;
+package br.com.gabriel.todolistchallenge.web;
 
 import br.com.gabriel.todolistchallenge.entity.ToDo;
 import br.com.gabriel.todolistchallenge.service.ToDoService;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,14 +23,19 @@ public class ToDoController {
         return service.list();
     }
     @PostMapping
-    public List<ToDo> create(@RequestBody @Valid ToDo toDo){
-        return service.create(toDo);
+    @Transactional
+    public ResponseEntity<List<ToDo>> create(@RequestBody @Valid ToDo toDo){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.create(toDo));
     }
-    @PutMapping
-    public List<ToDo> update(@RequestBody Long id, ToDo toDo){
-        return service.update(id, toDo);
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<List<ToDo>> update(@PathVariable Long id, @RequestBody ToDo toDo){
+        return ResponseEntity.status(HttpStatusCode.valueOf(200))
+                .body(service.update(id, toDo));
     }
     @DeleteMapping("/{id}")
+    @Transactional
     public List<ToDo> delete(@PathVariable("id") Long id){
         return service.delete(id);
     }
