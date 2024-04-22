063947dc354ad2c67326bd720969ada0249017de
Gabriel M. Assis <gabrielmedrs@icloud.com>
2024-04-22 12:47:27 -0300

    treating exceptions

--- src/main/java/br/com/gabriel/todolistchallenge/service/ToDoService.java
+++ src/main/java/br/com/gabriel/todolistchallenge/service/ToDoService.java
@@ -2,6 +2,7 @@ package br.com.gabriel.todolistchallenge.service;
 
 
 import br.com.gabriel.todolistchallenge.entity.ToDo;
+import br.com.gabriel.todolistchallenge.exception.BadRequestException;
 import br.com.gabriel.todolistchallenge.repository.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -25,14 +26,24 @@ public class ToDoService {
         return repository.findAll(sort);
     }
 
-    public List<ToDo> update(ToDo toDo){
-        repository.save(toDo);
+    public List<ToDo> update(Long id, ToDo toDo){
+        repository.findById(id).ifPresentOrElse((existingTodo) -> {
+            toDo.setId(id);
+            repository.save(toDo);
+        }, () -> {
+            throw new BadRequestException("ToDO %d not exist! ".formatted(id));
+        });
+
         return list();
+
     }
 
     public List<ToDo> delete(Long id){
-        repository.deleteById(id);
+        repository.findById(id).ifPresentOrElse((existingTodo) -> repository.delete(existingTodo), () -> {
+            throw new BadRequestException("Todo %d not exist! ".formatted(id));
+        });
         return list();
+
     }
 
 
--- src/main/java/br/com/gabriel/todolistchallenge/web/ToDoController.java
+++ src/main/java/br/com/gabriel/todolistchallenge/web/ToDoController.java
@@ -1,7 +1,8 @@
-package br.com.gabriel.todolistchallenge.controller;
+package br.com.gabriel.todolistchallenge.web.controller;
 
 import br.com.gabriel.todolistchallenge.entity.ToDo;
 import br.com.gabriel.todolistchallenge.service.ToDoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +19,12 @@ public class ToDoController {
         return service.list();
     }
     @PostMapping
-    public List<ToDo> create(@RequestBody ToDo toDo){
+    public List<ToDo> create(@RequestBody @Valid ToDo toDo){
         return service.create(toDo);
     }
     @PutMapping
-    public List<ToDo> update(@RequestBody ToDo toDo){
-        return service.update(toDo);
+    public List<ToDo> update(@RequestBody Long id, ToDo toDo){
+        return service.update(id, toDo);
     }
     @DeleteMapping("/{id}")
     public List<ToDo> delete(@PathVariable("id") Long id){
